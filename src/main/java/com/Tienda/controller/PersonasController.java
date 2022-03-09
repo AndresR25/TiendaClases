654da265a04313576c7ce210a35e96b5c3507789
Andres Rodriguez <kflex25@MacBook-Pro-de-Andres.local>
2022-03-08 21:47:01 -0600

    TiendaActualizadaaError

--- src/main/java/com/Tienda/controller/PersonasController.java
+++ src/main/java/com/Tienda/controller/PersonasController.java
@@ -8,17 +8,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonasController {
     @Autowired
     private iPersonaService personaService;
      
-    @GetMapping("/personas")
+    @GetMapping("/persona")
     public String personas(Model model){
     List<Persona> listaPersonas=personaService.getAllPerson();
     model.addAttribute("titulo","Personas");
     model.addAttribute("personas",listaPersonas);
     return "personas";
     }
+    
+    @GetMapping("/personasN")
+    public String crearPersona(Model model){
+        model.addAttribute("persona",new Persona());
+        return "crear";
+    }
+    
+    @PostMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona){
+        personaService.savePerson(persona);
+        return "redirect:/persona";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id") Long idPersona){
+        personaService.delete(idPersona);
+        return "redirect:/persona";
+    }
 }
+    
+    
+    
+ 
